1278bcded1cc9f40e824ea4b618bc5fcd7b7545a
priyakotabagi <priya.kotabagi@infosys.com>
2019-01-10 12:02:55 +0530

    added demo proj

---
+++ DemoProject/src/testProject/DemoClass.java
@@ -0,0 +1,16 @@
+package testProject;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		System.out.println("Before for loop");
+		for(i=0;i<10;i++) {
+			System.out.println("Welcome to Automation Unit");
+			
+		}
+
+	}
+
+}
