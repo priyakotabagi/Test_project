678c82d4dc0c2bff6c808e5cc4df5313c73d6ba3
priyakotabagi <priya.kotabagi@infosys.com>
2019-01-10 14:59:42 +0530

    added new project

---
+++ testproject/src/main/java/com/org/infy/mainproject/testproject/App.java
@@ -0,0 +1,18 @@
+package com.org.infy.mainproject.testproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int i=0;
+		System.out.println("Before for loop");
+		for(i=0;i<10;i++) {
+			System.out.println("Welcome to Automation Unit");
+			
+		}
+    }
+}
